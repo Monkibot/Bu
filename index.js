910f98257c88203c6f265a197ae6b9779e52900b
Monkibot <156599020+Monkibot@users.noreply.github.com>
2024-01-16 10:54:14 -0600

    index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const {name, author} = require(join(__dirname, './package.json'));
 const {say} = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
-say('Larios V2.0 - Bot\nWhatsApp Bot', {
+say('Dafne X Bu\nWhatsApp Bot', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']});
-say(`Bot creado por Larios.Psd`, {
+say(`Bot creado por Shadow`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']});
