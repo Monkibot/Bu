8e82eb36aaf2a817370386820f287ca4f07ae6ff
Monkibot <156599020+Monkibot@users.noreply.github.com>
2024-01-16 14:08:46 -0600

    Actualizar menus-menu.js

--- plugins/menus-menu.js
+++ plugins/menus-menu.js
@@ -394,7 +394,7 @@ ${usedPrefix}menu2
 ⚙️ ${usedPrefix}saveimage
 ⚙️ ${usedPrefix}viewimage
 
-    Costeño.X ⚙️ `.trim();
+    𝙱𝚄 𝚇 𝙳𝙰𝙵𝙽𝙴 ⚙️ `.trim();
     if (m.isGroup) {
       // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
       const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
